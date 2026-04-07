6c31dba9214312cc1d280f5e30ed745626d212e8
shiva2ziniosedge-gamedev <shiva2ziniosedge@gmail.com>
2026-04-07 12:08:30 +0530

    Update email service to outlook

--- Back-end/RamApi/obj/Debug/net8.0/RamApi.AssemblyInfo.cs
+++ Back-end/RamApi/obj/Debug/net8.0/RamApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RamApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62dfa6aa72a0e04534d2bf8db04c532f69b41bff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66e6ebf370dd74b1c96adf86bcfc814611690be6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RamApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RamApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
